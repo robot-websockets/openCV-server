c467c0481b812855fdd9bc6db4124b8c50a6dcdd
philstenning <philstenning@hotmail.com>
2019-02-01 12:56:27 +0000

    add args

--- camera.py
+++ camera.py
@@ -1,10 +1,29 @@
+import argparse
 import numpy as np
 import cv2
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-W', '--websocket_server')
+parser.add_argument('-V', '--video_server')
+
+args = parser.parse_args()
+
+web_socket_server = "192.168.55.18:5001"
+if (args.websocket_server):
+    web_socket_server = args.websocket_server
+
+video_server = "192.168.55.13:8081"
+if (args.video_server):
+    video_server = args.video_server
+
+print('\n video feed url: {} \n websocket server url:{}'.format(
+    video_server, web_socket_server))
+
+
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture("http://192.168.55.12:8081/")
+        self.video = cv2.VideoCapture("http://{}/".format(video_server))
 
     def __del__(self):
         self.video.release()
